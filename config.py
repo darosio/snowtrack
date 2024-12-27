78fee0606856aafcb40be956ac823f136f625c51
daniele arosio <daniele.arosio@cnr.it>
2024-12-27 17:16:57 +0100

    add migration

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
     # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///local.db')
-    SQLALCHEMY_DATABASE_URI = "sqlite:///local.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/local.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
